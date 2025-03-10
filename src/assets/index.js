7212a49f01722565e008b7bf36d17df1fef64d0d
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-03-10 11:21:51 +0800

    Portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,54 @@
+// Import company logos
+import mathwork from "./company/mathwork.png";
+import edunet from "./company/edunet.png";
+import eduskill from "./company/eduskill.png";
+
+// Import tech stack images
+
+// Import project images
+import weatherpedia from "./projects/WeatherPedia.png";
+import termpw from "./projects/termpw.png";
+import payloadmaster from "./projects/payloadmaster.png";
+import mhft from "./projects/mhft.png";
+import sketcher from "./projects/sketcher.png";
+import CompileVortex from "./projects/CompileVortex.png";
+
+// Import tech logos
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.png";
+
+// Import common assets
+import logo from "./logo.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  mathwork,
+  edunet,
+  github,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  mhft,
+  sketcher,
+  CompileVortex,
+  logo,
+  menu,
+  close,
+  eduskill,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,167 @@
+import {
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  edunet,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  threejs,
+  mhft,
+  sketcher,
+  mathwork,
+  CompileVortex,
+  eduskill,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  { title: "HTML 5", icon: html },
+  { title: "CSS 3", icon: css },
+  { title: "JavaScript", icon: javascript },
+  { title: "React JS", icon: reactjs },
+];
+
+export const technologies = [
+  { name: "HTML 5", icon: html },
+  { name: "CSS 3", icon: css },
+  { name: "JavaScript", icon: javascript },
+  { name: "Rect JS", icon: reactjs },
+  { name: "Tailwind CSS", icon: tailwind },
+  { name: "Node JS", icon: nodejs },
+  { name: "Three JS", icon: threejs },
+  { name: "git", icon: git },
+];
+
+export const experiences = [
+  {
+    title: "Software Engineer Intern",
+    company_name: "TechTalk",
+    icon: eduskill,
+    iconBg: "#161329",
+    date: "Feb 3 - Present",
+    points: [
+      "Developed hands-on experience in building scalable web applications using C# and ReactJS.",
+      "Strengthened problem-solving and software engineering skills through real-world development tasks and collaborative projects.",
+      "Gained practical knowledge of modern development workflows, including API integration, debugging, and code optimization."
+    ],    
+  },
+  {
+    title: "Software Engineer Intern",
+    company_name: "TechTalk",
+    icon: mathwork,
+    iconBg: "#161329",
+    date: "Feb 3 - Present",
+    points: [
+      "Developed hands-on experience in building scalable web applications using C# and ReactJS.",
+      "Strengthened problem-solving and software engineering skills through real-world development tasks and collaborative projects.",
+      "Gained practical knowledge of modern development workflows, including API integration, debugging, and code optimization."
+    ], 
+  },
+  {
+    title: "Software Engineer Intern",
+    company_name: "TechTalk",
+    icon: edunet,
+    iconBg: "#161329",
+    date: "Feb 3 - Present",
+    points: [
+      "Developed hands-on experience in building scalable web applications using C# and ReactJS.",
+      "Strengthened problem-solving and software engineering skills through real-world development tasks and collaborative projects.",
+      "Gained practical knowledge of modern development workflows, including API integration, debugging, and code optimization."
+    ], 
+  },
+];
+
+export const projects = [
+  {
+    name: "WeatherPedia",
+    description:
+      "Web-based platform that allows users to access weather information for their location by entering it in the search field",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "HTML", color: "green-text-gradient" },
+      { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
+      { name: "Weather API by API Ninjas", color: "yellow-text-gradient" },
+    ],
+    image: weatherpedia,
+    source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
+  },
+  {
+    name: "Terminal Like Portfolio Website",
+    description:
+      "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
+    tags: [
+      { name: "HTML", color: "blue-text-gradient" },
+      { name: "css", color: "green-text-gradient" },
+      { name: "Javascript", color: "pink-text-gradient" },
+    ],
+    image: termpw,
+    source_code_link: "https://github.com/lohitkolluri/lohitkolluri.github.io",
+  },
+  {
+    name: "Mental Health Fitness Tracker",
+    description:
+      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Jupyter Notebook", color: "green-text-gradient" },
+      { name: "Regression Algorithms", color: "pink-text-gradient" },
+    ],
+    image: mhft,
+    source_code_link:
+      "https://github.com/lohitkolluri/mental_health_fitness_tracker",
+  },
+  {
+    name: "PayloadMaster",
+    description:
+      "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "shell", color: "blue-text-gradient" },
+    ],
+    image: payloadmaster,
+    source_code_link: "https://github.com/lohitkolluri/PayloadMaster",
+  },
+  {
+    name: "CompileVortex",
+    description:
+      "Tool to automate payload creation using the Metasploit framework",
+    tags: [
+      { name: "Javascript", color: "blue-text-gradient" },
+      { name: "CSS", color: "green-text-gradient" },
+      { name: "HTML", color: "pink-text-gradient" },
+    ],
+    image: CompileVortex,
+    source_code_link: "https://github.com/lohitkolluri/CompileVortex",
+  },
+  {
+    name: "Sketcher",
+    description:
+      "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
+    tags: [
+      { name: "OpenCV", color: "blue-text-gradient" },
+      { name: "Matplotlib", color: "green-text-gradient" },
+      { name: "Python", color: "pink-text-gradient" },
+    ],
+
+    image: sketcher,
+    source_code_link: "https://github.com/lohitkolluri/Image_to_Pencil_Sketch_App",
+  },
+];
